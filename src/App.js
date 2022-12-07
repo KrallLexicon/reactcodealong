26a7e96e76dca12d5e1043f70653a1cfd4a5158f
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2022-12-07 10:08:02 +0100

    CodeAlong 221207

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import {Header} from './components/Header';
+import PersonList from './components/PersonList';
+import RemovePerson from './components/RemovePerson';
 
 function App() {
-  const dataValues = ['Apple', 'Banana', 'Potato']
+  // const dataValues = ['Apple', 'Banana', 'Potato']
   
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <h1>Hello potatis!</h1>
        {dataValues.map(items =>(
         <Header headerValue={items} />
        ))}
-      </header>
+      </header> */}
+     <PersonList/>
+     <RemovePerson/>
     </div>
   );
 }
